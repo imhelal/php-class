e9cc5c9e6814886c1af7435b9e829a5c4d163294
Helal <imhelal.dev@gmail.com>
2024-12-13 20:10:05 +0600

    Var and Data types

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-echo 'sfsdf';
+// PHP Var
+// PHP Data types
+// PHP Error
+
+$username = 'Jhon Deo';
+$number = 1;
+$number2 = 1.5;
+$isEnrolled = true; // Boolean
+$students = array( 'Habib', 'Mustafiz', 'Jubu' );
+
+$nullValue = null;
+
+var_dump( $nullValue );
+
+
+//echo $username;
 ?>
