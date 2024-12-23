46880652fe457b578f9d939e77f58856427b8d9b
Helal <imhelal.dev@gmail.com>
2024-12-23 19:46:50 +0600

    Form Handling

---
+++ 07-form/form-submit.php
@@ -0,0 +1,9 @@
+<?php 
+
+echo "<pre>";
+// print_r($_REQUEST);
+
+$submittedData = $_REQUEST;
+
+print_r($_FILES);
+
---
+++ 07-form/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Form</title>
+</head>
+<body>
+	
+
+
+<form action="form-submit.php" method="post" enctype="multipart/form-data">
+	<input type="text" placeholder="Name" name="username"><br/><br/>
+	<input type="text" placeholder="Email" name="email"><br/><br/>
+	<input type="file" placeholder="Email" name="photo"><br/><br/>
+	<input type="submit" value="Submit Form">
+</form>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
